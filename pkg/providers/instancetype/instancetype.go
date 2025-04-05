3a0bc106de59f18dd8fc4c6d6d09701382732092
Patryk Rostkowski <patrostkowski@gmail.com>
2025-04-05 21:20:33 +0200

    chore: implement instancetype boilerplate code
    Initial scaffolding for instancetype.Provider including
    stub methods and controller wiring. Also, adds example
    NodePool CRD and updates dependencies.
    
    Signed-off-by: Patryk Rostkowski <patrostkowski@gmail.com>

--- pkg/providers/instancetype/instancetype.go
+++ pkg/providers/instancetype/instancetype.go
@@ -18,7 +18,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
+	"math"
+	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -26,7 +28,11 @@ import (
 	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/patrickmn/go-cache"
 	"google.golang.org/api/iterator"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/karpenter/pkg/cloudprovider"
+	"sigs.k8s.io/karpenter/pkg/scheduling"
 
 	"github.com/cloudpilot-ai/karpenter-provider-gcp/pkg/apis/v1alpha1"
 	"github.com/cloudpilot-ai/karpenter-provider-gcp/pkg/auth"
@@ -36,46 +42,98 @@ import (
 const (
 	InstanceTypesCacheKey = "gce-instancetypes"
 	InstanceTypesCacheTTL = 23 * time.Hour
+	oneMiB                = 1024 * 1024
 )
 
 type Provider interface {
-	LivenessProbe(*http.Request) error
-	List(context.Context, *v1alpha1.KubeletConfiguration, *v1alpha1.GCENodeClass) ([]*cloudprovider.InstanceType, error)
+	List(context.Context, *v1alpha1.GCENodeClass) ([]*cloudprovider.InstanceType, error)
 	UpdateInstanceTypes(ctx context.Context) error
 	UpdateInstanceTypeOfferings(ctx context.Context) error
 }
 
 type DefaultProvider struct {
-	authOptions   *auth.Credential
-	vmTypesClient *compute.MachineTypesClient
+	authOptions        *auth.Credential
+	machineTypesClient *compute.MachineTypesClient
 
 	muCache sync.Mutex
 	cache   *cache.Cache
 }
 
-func NewProvider(region string, authOptions *auth.Credential) (*DefaultProvider, error) {
-	vmTypesClient, err := compute.NewMachineTypesRESTClient(context.Background(), authOptions.CredentialOptions)
+func NewDefaultProvider(ctx context.Context, authOptions *auth.Credential) *DefaultProvider {
+	machineTypesClient, err := compute.NewMachineTypesRESTClient(ctx)
 	if err != nil {
-		return nil, err
+		log.FromContext(ctx).Error(err, "failed to create default provider for node pool template")
+		os.Exit(1)
 	}
 	return &DefaultProvider{
-		authOptions:   authOptions,
-		vmTypesClient: vmTypesClient,
-		cache:         cache.New(InstanceTypesCacheTTL, pkgcache.DefaultCleanupInterval),
-	}, nil
+		authOptions:        authOptions,
+		machineTypesClient: machineTypesClient,
+		cache:              cache.New(InstanceTypesCacheTTL, pkgcache.DefaultCleanupInterval),
+	}
 }
 
-func (p *DefaultProvider) LivenessProbe(*http.Request) error {
-	// TODO: Implement me
-	return nil
-}
-
-func (p *DefaultProvider) List(ctx context.Context, kc *v1alpha1.KubeletConfiguration, nodeClass *v1alpha1.GCENodeClass) ([]*cloudprovider.InstanceType, error) {
-	// TODO: Implement me
+func (p *DefaultProvider) List(ctx context.Context, nodeClass *v1alpha1.GCENodeClass) ([]*cloudprovider.InstanceType, error) {
 	p.muCache.Lock()
 	defer p.muCache.Unlock()
 
-	return nil, nil
+	vmTypes, err := p.getInstanceTypes(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("listing instance types: %w", err)
+	}
+
+	instanceTypes := []*cloudprovider.InstanceType{}
+	for _, mt := range vmTypes {
+		if mt.Name == nil || mt.MemoryMb == nil || mt.GuestCpus == nil {
+			continue
+		}
+
+		memory := int64(*mt.MemoryMb) * 1024 * 1024 // MiB -> Bytes
+		cpu := int64(*mt.GuestCpus)
+		isSharedCore := strings.Contains(*mt.Name, "e2-micro") || strings.Contains(*mt.Name, "e2-small") || strings.Contains(*mt.Name, "e2-medium")
+
+		kubeMem, evictionMem := calculateMemoryOverheadBreakdown(memory)
+		overhead := cloudprovider.InstanceTypeOverhead{
+			KubeReserved: corev1.ResourceList{
+				corev1.ResourceCPU:    calculateCPUOverhead(cpu, isSharedCore),
+				corev1.ResourceMemory: kubeMem,
+			},
+			EvictionThreshold: corev1.ResourceList{
+				corev1.ResourceMemory: evictionMem,
+			},
+			SystemReserved: corev1.ResourceList{},
+		}
+
+		instanceTypes = append(instanceTypes, &cloudprovider.InstanceType{
+			Name: *mt.Name,
+			Capacity: corev1.ResourceList{
+				corev1.ResourceCPU:    *resource.NewQuantity(cpu, resource.DecimalSI),
+				corev1.ResourceMemory: *resource.NewQuantity(memory, resource.BinarySI),
+			},
+			Overhead:     &overhead,
+			Requirements: scheduling.NewRequirements(),
+		})
+	}
+
+	log.FromContext(ctx).WithValues("count", len(instanceTypes)).Info("listed GCE instance types")
+	return instanceTypes, nil
+}
+
+func (p *DefaultProvider) UpdateInstanceTypeOfferings(ctx context.Context) error {
+	types, err := p.getInstanceTypes(ctx)
+	if err != nil {
+		return fmt.Errorf("getting instance type offerings: %w", err)
+	}
+	log.FromContext(ctx).WithValues("count", len(types)).Info("updated instance type offerings from aggregated list")
+	return nil
+}
+
+func (p *DefaultProvider) UpdateInstanceTypes(ctx context.Context) error {
+	types, err := p.getInstanceTypes(ctx)
+	if err != nil {
+		return fmt.Errorf("getting instance types: %w", err)
+	}
+	log.FromContext(ctx).WithValues("count", len(types)).Info("discovered GCE instance types")
+	return nil
 }
 
 func (p *DefaultProvider) getInstanceTypes(ctx context.Context) ([]*computepb.MachineType, error) {
@@ -92,7 +150,7 @@ func (p *DefaultProvider) getInstanceTypes(ctx context.Context) ([]*computepb.Ma
 		Filter:  &vmFilter,
 	}
 
-	it := p.vmTypesClient.AggregatedList(ctx, req)
+	it := p.machineTypesClient.AggregatedList(ctx, req)
 
 	var vmTypes []*computepb.MachineType
 	for {
@@ -110,4 +168,70 @@ func (p *DefaultProvider) getInstanceTypes(ctx context.Context) ([]*computepb.Ma
 
 	p.cache.SetDefault(InstanceTypesCacheKey, vmTypes)
 	return vmTypes, nil
+}
+
+// calculated with https://cloud.google.com/kubernetes-engine/docs/concepts/plan-node-sizes#memory_reservations
+func calculateMemoryOverheadBreakdown(totalMemoryBytes int64) (kubeReserved, evictionThreshold resource.Quantity) {
+	mib := float64(totalMemoryBytes) / oneMiB
+	var reserved float64
+
+	if mib < 1024 {
+		reserved = 255
+	} else {
+		memLeft := mib
+
+		step := math.Min(memLeft, 4096)
+		reserved += step * 0.25
+		memLeft -= step
+
+		step = math.Min(memLeft, 4096)
+		reserved += step * 0.20
+		memLeft -= step
+
+		step = math.Min(memLeft, 8192)
+		reserved += step * 0.10
+		memLeft -= step
+
+		step = math.Min(memLeft, 114688)
+		reserved += step * 0.06
+		memLeft -= step
+
+		if memLeft > 0 {
+			reserved += memLeft * 0.02
+		}
+	}
+
+	return *resource.NewQuantity(int64(reserved*oneMiB), resource.BinarySI),
+		*resource.NewQuantity(100*oneMiB, resource.BinarySI)
+}
+
+// calculated with https://cloud.google.com/kubernetes-engine/docs/concepts/plan-node-sizes#cpu_reservations
+func calculateCPUOverhead(vcpus int64, isSharedCore bool) resource.Quantity {
+	if isSharedCore {
+		return resource.MustParse("1060m")
+	}
+
+	var reservedMillicores float64
+
+	if vcpus >= 1 {
+		// 6% of first core
+		reservedMillicores += 1000 * 0.06
+	}
+
+	if vcpus >= 2 {
+		// 1% of next core
+		reservedMillicores += 1000 * 0.01
+	}
+
+	if vcpus >= 4 {
+		// 0.5% of next 2 cores
+		reservedMillicores += 2000 * 0.005
+	}
+
+	if vcpus > 4 {
+		// 0.25% of remaining
+		reservedMillicores += float64(vcpus-4) * 1000 * 0.0025
+	}
+
+	return *resource.NewMilliQuantity(int64(reservedMillicores), resource.DecimalSI)
 }
