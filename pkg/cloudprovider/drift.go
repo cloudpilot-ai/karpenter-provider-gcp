fbb229ce67c07b3f430c7ced82a13cbb705673cc
Asklv <boironic@gmail.com>
2025-05-16 17:45:07 +0800

    feat: support drifted operation.
    Signed-off-by: Asklv <boironic@gmail.com>

--- pkg/cloudprovider/drift.go
+++ pkg/cloudprovider/drift.go
@@ -15,9 +15,53 @@ limitations under the License.
 package cloudprovider
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/samber/lo"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 	"sigs.k8s.io/karpenter/pkg/cloudprovider"
+
+	"github.com/cloudpilot-ai/karpenter-provider-gcp/pkg/apis/v1alpha1"
+	"github.com/cloudpilot-ai/karpenter-provider-gcp/pkg/providers/instance"
 )
 
 const (
 	NodeClassDrift cloudprovider.DriftReason = "NodeClassDrift"
 )
+
+func (c *CloudProvider) isNodeClassDrifted(ctx context.Context, nodeClaim *karpv1.NodeClaim, _ *karpv1.NodePool, nodeClass *v1alpha1.GCENodeClass) (cloudprovider.DriftReason, error) {
+	if drifted := c.staticFieldsDrifted(nodeClaim, nodeClass); drifted != "" {
+		err := errors.New("current node claim is drifted")
+		log.FromContext(ctx).Error(err, "drifted", drifted)
+		return drifted, err
+	}
+	// TODO: wait for GCENodeClassStatus is ready
+	return "", nil
+}
+
+func (c *CloudProvider) staticFieldsDrifted(nodeClaim *karpv1.NodeClaim, nodeClass *v1alpha1.GCENodeClass) cloudprovider.DriftReason {
+	nodeClassHash, foundNodeClassHash := nodeClass.Annotations[v1alpha1.AnnotationGCENodeClassHash]
+	nodeClassHashVersion, foundNodeClassHashVersion := nodeClass.Annotations[v1alpha1.AnnotationGCENodeClassHashVersion]
+	nodeClaimHash, foundNodeClaimHash := nodeClaim.Annotations[v1alpha1.AnnotationGCENodeClassHash]
+	nodeClaimHashVersion, foundNodeClaimHashVersion := nodeClaim.Annotations[v1alpha1.AnnotationGCENodeClassHashVersion]
+
+	if !foundNodeClassHash || !foundNodeClaimHash || !foundNodeClassHashVersion || !foundNodeClaimHashVersion {
+		return ""
+	}
+	// validate that the hash version for the GCENodeClass is the same as the NodeClaim before evaluating for static drift
+	if nodeClassHashVersion != nodeClaimHashVersion {
+		return ""
+	}
+	return lo.Ternary(nodeClassHash != nodeClaimHash, NodeClassDrift, "")
+}
+
+func (c *CloudProvider) getInstance(ctx context.Context, providerID string) (*instance.Instance, error) {
+	instance, err := c.instanceProvider.Get(ctx, providerID)
+	if err != nil {
+		return nil, fmt.Errorf("getting instance, %w", err)
+	}
+	return instance, nil
+}
