ee1490848e81efdd43222f3351f4b910e3e37d59
Igor Korniienko <kornienko.igor@gmail.com>
2025-09-26 16:50:28 -0700

    feat: add networkTags support to GCENodeClass and merge instance tags functionality
    Signed-off-by: Igor Korniienko <kornienko.igor@gmail.com>

---
+++ pkg/providers/instance/instance_test.go
@@ -0,0 +1,43 @@
+package instance
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/api/compute/v1"
+)
+
+func TestMergeInstanceTagsPreservesTemplateAndAddsNetworkTags(t *testing.T) {
+	base := &compute.Tags{Items: []string{"gke-default", "existing"}, Fingerprint: "fp"}
+
+	merged := mergeInstanceTags(base, []string{"existing", "custom", " custom-spaced "})
+
+	require.NotNil(t, merged)
+	require.Equal(t, []string{"gke-default", "existing", "custom", "custom-spaced"}, merged.Items)
+	require.Equal(t, "fp", merged.Fingerprint)
+	require.Equal(t, []string{"gke-default", "existing"}, base.Items, "base tags should not be mutated")
+	require.NotSame(t, base, merged)
+}
+
+func TestMergeInstanceTagsHandlesNilTemplate(t *testing.T) {
+	merged := mergeInstanceTags(nil, []string{"tag-one", "tag-two"})
+
+	require.NotNil(t, merged)
+	require.Equal(t, []string{"tag-one", "tag-two"}, merged.Items)
+	require.Empty(t, merged.Fingerprint)
+}
+
+func TestMergeInstanceTagsFiltersEmptyNetworkTags(t *testing.T) {
+	base := &compute.Tags{Items: []string{"base"}}
+
+	merged := mergeInstanceTags(base, []string{"", "   ", "extra"})
+
+	require.NotNil(t, merged)
+	require.Equal(t, []string{"base", "extra"}, merged.Items)
+}
+
+func TestMergeInstanceTagsReturnsNilWhenNoTags(t *testing.T) {
+	require.Nil(t, mergeInstanceTags(nil, nil))
+	require.Nil(t, mergeInstanceTags(&compute.Tags{}, nil))
+	require.Nil(t, mergeInstanceTags(nil, []string{"   "}))
+}
